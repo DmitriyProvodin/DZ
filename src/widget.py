51e73ebf8ee2b3cdc75502f230af9508fd1d90a6
Dmitrou <provodind@bk.ru>
2025-03-19 07:06:14 +0300

    add sr\widget.py

--- src/widget.py
+++ src/widget.py
@@ -2,17 +2,19 @@ from . import masks
 
 
 def mask_account_card(nums: str) -> str:
-    if 'Счёт' in nums:
+    if "Счёт" in nums:
         return masks.get_mask_account(nums)
     else:
         cards = masks.get_mask_card_number(nums[-16:])
         new_card = nums.replace(nums[-16:], cards)
         return new_card
 
-print(mask_account_card('Visa Platinum 7000792289606361'))
+
+print(mask_account_card("Visa Platinum 7000792289606361"))
 
 
-def get_data(date:str) -> str:
-    return f'{date[8:10]}.{date[5:7]}.{date[0:4]}'
+def get_data(date: str) -> str:
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
 
-print(get_data('2024-03-11T02:26:18.671407'))
+
+print(get_data("2024-03-11T02:26:18.671407"))
