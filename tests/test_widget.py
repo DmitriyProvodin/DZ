be6b9b22f659bb279a52348d0de8242ea4fcfa4b
Dmitrou <provodind@bk.ru>
2025-04-07 07:54:25 +0300

    исправление ошибки с функцией get_data?

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,5 +1,5 @@
 import pytest
-from src.widget import mask_account_card, get_date
+from src.widget import mask_account_card, get_data
 
 
 @pytest.mark.parametrize(
@@ -40,9 +40,9 @@ def empty_mask_account_card(user_input, expected):
 
 @pytest.fixture
 def right_get_date():
-    assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
+    assert get_data("2024-03-11T02:26:18.671407") == "11.03.2024"
 
 
 @pytest.fixture
 def test_get_date():
-    assert get_date("") == "Нет данных о дате"
+    assert get_data("") == "Нет данных о дате"
